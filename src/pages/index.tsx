674c137a63f3b299e16afdde70fa5148c2a981e3
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-02-24 01:47:18 -0300

    create app to next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import ExperienceBar from '../components/ExperienceBar';
+ 
+export default function Home() {
+  return (
+    <Container>
+      <ExperienceBar />
+    </Container>
+  );
+}
