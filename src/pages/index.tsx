f491c410f53213270a4ff1ecd7e7100c5155e62b
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-02-25 02:41:17 -0300

    component countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import { Container } from '../styles/pages/home';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
+import CompletedChallenges from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
  
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <Container>
+      <Head>
+        <title>Início | Moveit</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
-          <Profile></Profile>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
